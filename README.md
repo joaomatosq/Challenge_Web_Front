dc6056887501874bdeb4fd4a1f0f82609c037040
joaomatosq <joaomatosq@gmail.com>
2024-11-29 22:46:10 -0300

    Ajustando o uso do javascript e adicionado conteudo no readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# Bem vindos ao Grupo Moskitto GS 🦟
+# Challenge - Web Development e Front-end Design 
+
+# Painel Remoto para o Hospital Infantil Sabará
+
+## Trabalho realizado para o "Challenge" na instituição FIAP
+### Matéria de Web Development - Professor Wellington Cidade
+### Matéria de Front-end - Professor Marcelo Amorim
+
+## Membros do Grupo:
+
+- [João Vitor de Matos](https://github.com/joaomatosq) - RM559246
+  
+- [Gabriel Couto](https://github.com/rouri404) - RM559579
+
+- [Gabriel Kato](https://github.com/kato8088) - RM560000
+
+- [Ana Luiza](https://github.com/anarand) - RM
+
+## Links:
+
+
+
+
+
---
+++ src/js/script-index.js
@@ -0,0 +1,39 @@
+function alterarSenhaPaciente() {
+    var senhaPaciente = document.getElementById("input-paciente").value;
+
+    if (senhaPaciente === "") {
+        document.getElementById("painel-paciente").innerText = "Digite um Valor!";
+        return; 
+    }
+
+    var valorSenha = parseInt(senhaPaciente, 10);
+
+    if (valorSenha > 1000) {
+        alert("Digite um número menor que 1000!");
+    } else {
+        document.getElementById("painel-paciente").innerText = valorSenha;
+    }
+
+
+
+    //Região na qual futuramente poderá ser feito uma automatização 
+    //para os funcionários colocarem a próxima senha e sala.
+    var senhaFuncionario = 12;
+    if(valorSenha == senhaFuncionario) {
+        document.body.innerHTML = `
+            <div id="mensagem-centralizada">
+                <h1>Compareça à sala [ ] </h1>
+            </div>
+        `;
+        document.body.style.cssText = `
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: serif;
+            color: #FD000D;
+        `;
+    }
+
+}
